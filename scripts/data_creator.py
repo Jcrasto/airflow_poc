edefd4f4f28e13166713539733a43b057d3729cf
joshcrasto <jcrasto@g.ucla.edu>
2021-09-23 20:22:29 -0700

    test

---
+++ scripts/data_creator.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+import numpy
+
+
+if __name__ == "__main__":
+    now = datetime.now()
+    current_date = now.strftime("%Y-%m-%d")
+    file = open("../files/normal_dist_sample_{}.csv".format(current_date), "w")
+    for i in range(100):
+        file.write(str(numpy.random.normal(0,1)) + "," + str(numpy.random.normal(0,1)) + "\n")
+
