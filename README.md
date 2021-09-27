aad2c21eede6024e520748b99a0e5a4623dcdd4a
joshcrasto <jcrasto@g.ucla.edu>
2021-09-26 19:31:18 -0700

    pushing up changes to the dag_test, dag still not running properly

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # airflow_poc
 docker run -d -p 8080:8080 puckel/docker-airflow webserver
-docker run -d -p 8080:8080 -v /Users/jcrasto/airflow_poc:/usr/local/airflow/dags  puckel/docker-airflow webserver
+docker run -d -p 8080:8080 -v /Users/jcrasto/airflow_poc/dags:/usr/local/airflow/dags  puckel/docker-airflow webserver
 
 http://localhost:8080/admin/
--- dags/dag_test.py
+++ dags/dag_test.py
@@ -18,4 +18,19 @@ dag = DAG('dag_test', default_args=default_args)
 
 t1 = BashOperator(
     task_id='task_1',
-    bash_command='python /Users/jcrasto/airflow_poc/scripts/data_creator.py')
+    bash_command='/Users/jcrasto/airflow_poc/venv/bin/python /Users/jcrasto/airflow_poc/scripts/data_creator.py',
+    dag=dag)
+
+
+t2 = BashOperator(
+    task_id='task_2',
+    bash_command='/Users/jcrasto/airflow_poc/venv/bin/python /Users/jcrasto/airflow_poc/scripts/s3_uploader.py',
+    dag=dag)
+
+t3 = BashOperator(
+    task_id='task_3',
+    bash_command='/Users/jcrasto/airflow_poc/venv/bin/python /Users/jcrasto/airflow_poc/scripts/file_folder_cleanup.py',
+    dag=dag)
+
+
+t1 >> t2 >> t3
